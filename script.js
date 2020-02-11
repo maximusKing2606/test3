93c8489e135a1be309a9fd7e132b3865c01692f6
maximusKing2606 <hripamaksa@gmail.com>
2020-02-11 19:57:22 +0200

    last commit

---
+++ script.js
@@ -0,0 +1,70 @@
+$(function(){
+
+    
+    let header = $("#header"),
+
+    introH = $("#intro").innerHeight(),
+
+    scrollOffset = $(window).scrollTop();
+
+
+// Fixed Header
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function(){
+
+     scrollOffset = $(this).scrollTop();
+
+     checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+if (scrollOffset >= introH) {
+    header.addClass("fixed");
+} else{
+    header.removeClass("fixed");
+}
+}
+
+
+// Smoth scroll
+
+$("[data-scroll]").on("click", function(event){
+event.preventDefault();
+
+let $this = $(this),
+ blockId = $this.data('scroll'),
+blockOffset = $(blockId).offset().top;
+
+$("#nav a").removeClass("active");
+$this.addClass("active");
+
+$("html,body").animate({
+    scrollTop: blockOffset
+},1000)
+});
+
+// nav togle
+
+$("#nav__togle").on("click", function(event){
+event.preventDefault();
+
+$(this).toggleClass("active")
+$("#nav").toggleClass("active");
+})
+
+// collapse
+
+$("[data-collapse]").on("click",function(event){
+event.preventDefault();
+
+let $this = $(this),
+blockId = $this.data('collapse');
+
+$this.toggleClass("active")
+})
+
+});
+
+
+
